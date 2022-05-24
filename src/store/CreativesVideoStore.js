ef1d8756dd0254fe7f68c8d6de1351573d6aa129
TuduJ <jitendratd@gmail.com>
2022-05-24 14:17:18 +0530

    CreativesVideoStore

--- src/store/CreativesVideoStore.js
+++ src/store/CreativesVideoStore.js
@@ -1,12 +1,14 @@
-import { action } from 'mobx';
+import { action, makeObservable } from 'mobx';
 import API from '../api';
 
 class CreativesVideoStore {
-  constructor(rootStore) {
-    this.rootStore = rootStore;
+  constructor() {
+    makeObservable(this, {
+      getVideoUrl: action,
+    })
   }
 
-  @action async getVideoUrl(creativeId) {
+  getVideoUrl(creativeId) {
     this.rootStore.uiStore.isLoading = true;
     try {
       const response = await API.get(`/pilot/get_ftp_creative_files/?item_id=${creativeId}`);
