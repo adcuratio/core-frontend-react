22883c5bc13f0ba62e01331fbca931f70dfa574f
sonali-rana <sonalirana597@gmail.com>
2022-06-15 17:25:45 +0530

    added agency routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,3 +1,5 @@
+import Reports from "../reports/Reports";
+
 // Univision-Network-portal
 import UnivisionLanding from "../dash-univision/UnivisionLanding";
 import UnivisionOrderManagement from "../dash-univision/univsion-agg-campaign/pages/OrderManagementLanding";
@@ -35,6 +37,30 @@ import UnivisionAddressableInventory from "../dash-univision/view-schedules/view
 import UnivisionAddressableLogs from "../dash-univision/view-schedules/view-schedule/ViewSchedulesLogs";
 import CampaignDrafts from "../dash-univision/univsion-agg-campaign/agg-campaign-drafts/CampaignDrafts";
 
+// Xandr
+import XandrLanding from "../dash-xandr/XandrLanding";
+import XandrCreatives from "../dash-xandr/manage-creatives/ManageCreatives";
+import XandrSegmentFiles from "../dash-xandr/segment-files/SegmentFiles";
+import XandrManageOrders from "../dash-xandr/manage-orders/ManageOrders";
+import XandrPostLogs from "../dash-xandr/post-logs/XandrPostLogs";
+
+// Vizio-Portal
+import VizioLanding from "../dash-vizio/VizioLanding";
+import VizioManageOrders from "../dash-vizio/manage-orders/ManageOrders";
+import VizioTargetSegments from "../dash-vizio/target-files/TargetSegments";
+import VizioReviewCreatives from "../dash-vizio/creatives/creatives-review/ReviewCreatives";
+import VizioCreativesWatermarkApproval from "../dash-vizio/creatives/creatives-watermark-approval/CreativesWatermarkApproval";
+
+// CBS-Network-portal
+import CbsLanding from "../dash-cbs/CBSLanding";
+import CBSNetworkLogs from "../dash-cbs/cbs-network-logs/CBSNetworkLogs";
+import CbsNetworkAdvertiserSchedule from "../dash-cbs/advertiser-schedule";
+import CbsManageChannels from "./dash-cbs/network-manage-channels/NetworkManageChannels";
+import CbsReviewCreatives from "../dash-cbs/creatives/creatives-review/ReviewCreatives";
+import CbsManageCreatives from "../dash-cbs/creatives/creatives-manage/ManageCreatives";
+import CbsCreativesWatermarkApproval from "../dash-cbs/creatives/creatives-watermark-approval/CreativesWatermarkApproval";
+import CbsPingReports from "../dash-cbs/tv-ping-reports/PingReports";
+
 const UnivsionAdminRoutes = [
   {
     path: "dash.univisionLanding",
@@ -45,6 +71,181 @@ const UnivsionAdminRoutes = [
   },
 ];
 
-const routes = [...UnivsionAdminRoutes];
+const XandrAdminRoutes = [
+  {
+    path: "dash.xandrLanding",
+    url: "/xandr-distributor-admin/landing",
+    name: "Xandr Landing page",
+    roles: "",
+    component: XandrLanding,
+  },
+  {
+    path: "dash.xandrCreatives",
+    url: "/xandr-distributor-admin/manage-creatives",
+    name: "",
+    roles: "",
+    component: XandrCreatives,
+  },
+  {
+    path: "dash.xandrSegmentFiles",
+    url: "/xandr-distributor-admin/segment-files",
+    name: "",
+    roles: "",
+    component: XandrSegmentFiles,
+  },
+  {
+    path: "dash.xandrManageOrders",
+    url: "/xandr-distributor-admin/manage-orders",
+    name: "",
+    roles: "",
+    component: XandrManageOrders,
+  },
+  {
+    path: "dash.xandrPostLogs",
+    url: "/xandr-distributor-admin/post-logs",
+    name: "",
+    roles: "",
+    component: XandrPostLogs,
+  },
+  {
+    path: "dash.xandrBillingReports",
+    url: "/xandr-distributor-admin/billing-reports",
+    name: "",
+    roles: "",
+    component: Reports,
+  },
+  {
+    path: "dash.xandrBillingTrends",
+    url: "/xandr-distributor-admin/billing-trends",
+    name: "",
+    roles: "",
+    component: Reports,
+  },
+  {
+    path: "dash.xandrCampaignReports",
+    url: "/xandr-distributor-admin/campaign-reports",
+    name: "",
+    roles: "",
+    component: Reports,
+  },
+];
+
+const VizioAdminRoutes = [
+  {
+    path: "dash.vizioLanding",
+    url: "/vizio-distributor-admin/landing",
+    name: "Vizio Landing page",
+    roles: "",
+    component: VizioLanding,
+  },
+  {
+    path: "dash.vizioManageOrders",
+    url: "/vizio-distributor-admin/manage-orders",
+    name: "",
+    roles: "",
+    component: VizioManageOrders,
+  },
+  {
+    path: "dash.vizioTargetSegments",
+    url: "/vizio-distributor-admin/target-segments",
+    name: "",
+    roles: "",
+    component: VizioTargetSegments,
+  },
+  {
+    path: "dash.vizioReviewCreatives",
+    url: "/vizio-admin/review-creatives",
+    name: "",
+    roles: "",
+    component: VizioReviewCreatives,
+  },
+  {
+    path: "dash.vizioCreativesWatermarkApproval",
+    url: "/vizio-admin/creatives-watermark-approval",
+    name: "",
+    roles: "",
+    component: VizioCreativesWatermarkApproval,
+  },
+];
+
+const CbsAdminRoutes = [
+  {
+    path: "dash.cbsNetworkLanding",
+    url: "/cbs-admin/ncm-landing",
+    name: "CBS Landing page",
+    roles: "",
+    component: CbsLanding,
+  },
+  {
+    path: "dash.cbsAddTraffickingPlan",
+    url: "/cbs-admin/network-logs",
+    name: "",
+    roles: "",
+    component: CBSNetworkLogs,
+  },
+  {
+    path: "dash.cbsNetworkEdiTable",
+    url: "/cbs-admin/edi-list",
+    name: "",
+    roles: "",
+    component: CbsNetworkAdvertiserSchedule,
+  },
+  {
+    path: "dash.cbsManageChannels",
+    url: "/cbs-admin/networks",
+    name: "",
+    roles: "",
+    component: CbsManageChannels,
+  },
+  {
+    path: "dash.cbsReviewCreatives",
+    url: "/cbs-admin/review-creatives",
+    name: "",
+    roles: "",
+    component: CbsReviewCreatives,
+  },
+  {
+    path: "dash.cbsManageCreatives",
+    url: "/cbs-admin/manage-creatives",
+    name: "",
+    roles: "",
+    component: CbsManageCreatives,
+  },
+  {
+    path: "dash.cbsCreativesWatermarkApproval",
+    url: "/cbs-admin/creatives-watermark-approval",
+    name: "",
+    roles: "",
+    component: CbsCreativesWatermarkApproval,
+  },
+  {
+    path: "dash.cbsPingReports",
+    url: "/cbs-admin/tv-ping-reports",
+    name: "",
+    roles: "",
+    component: CbsPingReports,
+  },
+  {
+    path: "dash.cbsDeliveryReports",
+    url: "/cbs-admin/delivery-reports",
+    name: "",
+    roles: "",
+    component: Reports,
+  },
+  {
+    path: "dash.cbsCampaignTvPingReports",
+    url: "/cbs-admin/campaign-tv-ping",
+    name: "",
+    roles: "",
+    component: Reports,
+  },
+];
+
+const routes = [
+  ...UnivsionAdminRoutes,
+  ...XandrAdminRoutes,
+  ...VizioAdminRoutes,
+  ...CbsAdminRoutes,
+];
 
 export default routes;
